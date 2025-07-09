b6e9855d4abb10430a2192f3dc4ff64214691da2
Daniel Nunes Teixeira <nunesteixeiradaniel12@gmail.com>
2025-07-09 15:55:11 -0300

    removing padding. Just that.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 
+
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen p-5">
-        <div className="w-full h-full p-5 flex row border-2 border-solid border-[var(--foreground)]">
-	    <div className="w-full p-5 overflow-y-scroll border-2 border-solid border-[var(--foreground)]">
-	        <div className="h-full">
-	            Content
+    <div className="w-screen h-screen">
+        <div className="w-full h-full flex row border-2 border-solid border-[var(--foreground)]">
+	    <div className="w-full overflow-y-scroll border-2 border-solid border-[var(--foreground)]">
+	        <div className="h-full p-5">
+	            Content.
 		</div>
-		<div className="w-full h-80 border-2 border-solid border-[var(--foreground)]">
-		    Footer
+		<div className="w-full p-5 h-80 border-2 border-solid border-[var(--foreground)]">
+		    Footer.
 		</div>
 	    </div>
 	    <div id="navigator-panel" className="p-5 border-2 border-solid border-[var(--foreground)]">
-	        Navigator
+	        Navigator.
 	    </div>
 	    <div id="navigator-button" className="right-10 fixed p-5 pt-6 pb-6 rounded-full border-2 border-solid border-[var(--foreground)]">
 	        {/* TODO: navigator button for mobile devices */}
